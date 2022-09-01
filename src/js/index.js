399f0f22040acf418c17d86bb3a36f206ad581c5
DmitriySir <109790162+DmitriySir@users.noreply.github.com>
2022-09-01 18:32:39 +0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../js/script.js';
+import '../js/closebtn';
+import '../scss/style.scss';
+import '../../node_modules/swiper/swiper-bundle.js'
+import '../js/modal.js';
+import '../js/showmore.js';
+
+
+ 
