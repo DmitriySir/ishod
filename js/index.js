f3314a2922ffaffda0c1856f5101db31b36f58eb
DmitriySir <109790162+DmitriySir@users.noreply.github.com>
2022-09-01 18:30:44 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import '../js/script.js';
-import '../js/closebtn';
-import '../scss/style.scss';
-import '../../node_modules/swiper/swiper-bundle.js'
-import '../js/modal.js';
-import '../js/showmore.js';
-
-
- 
